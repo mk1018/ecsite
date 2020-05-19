a8d075a3ea10d5dffbb0341ef30f87f32312d234
mk1018 <ktmn1018@yahoo.co.jp>
2020-05-19 16:54:27 +0900

    study_4-7( カード編集 )

--- ecsite/routes/web.php
+++ ecsite/routes/web.php
@@ -15,7 +15,8 @@ Route::get('/','ItemController@Index');
 Route::get('/item/{item}','ItemController@show');
 Route::post('/cartitem','CartItemController@store');
 Route::get('/cartitem', 'CartItemController@index');
-
+Route::delete('/cartitem/{cartItem}', 'CartItemController@destroy');
+Route::put('/cartitem/{cartItem}', 'CartItemController@update');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
