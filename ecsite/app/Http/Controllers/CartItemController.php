090ee71a44eb5ba461784de63da7f0e3d6b5f01e
mk1018 <ktmn1018@yahoo.co.jp>
2020-05-19 16:50:09 +0900

    study_4-6( カード内容画面 )

--- ecsite/app/Http/Controllers/CartItemController.php
+++ ecsite/app/Http/Controllers/CartItemController.php
@@ -15,7 +15,18 @@ class CartItemController extends Controller
      */
     public function index()
     {
-        //
+        $cartitems = CartItem::select('cart_items.*', 'items.name', 'items.amount')
+            ->where('user_id', Auth::id())
+            ->join('items', 'items.id','=','cart_items.item_id')
+            ->get();
+
+        $subtotal = 0;
+        
+        foreach($cartitems as $cartitem){
+            $subtotal += bcmul( $cartitem->amount , $cartitem->quantity , 0);
+        }
+        
+        return view('cartitem/index', ['cartitems' => $cartitems, 'subtotal' => $subtotal]);
     }
 
     /**
